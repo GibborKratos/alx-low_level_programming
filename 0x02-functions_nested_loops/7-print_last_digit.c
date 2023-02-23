fdf60205c537bafcb24abc73d59612a7637299b0
GibborKratos <gibbsonanita@gmail.com>
2023-02-22 19:48:24 -0800

    7-main

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,14 @@
   */
 int print_last_digit(int n);
 {
-	int l;
-	l = n % 10;
-	if (l < 0)
+
+	int last;
+
+	last = n % 10;
+	if (last < 0)
 	{
-		_putchar(-l + 48);
-		return (-l);
+		last = last * -1;
 	}
-	else
-	{
-		_putchar(l + 48);
-		return (l);
-	}
+	_putchar(last + '0');
+		return (last);
 }
