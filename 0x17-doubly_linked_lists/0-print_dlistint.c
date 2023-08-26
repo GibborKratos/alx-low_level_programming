2d6a4ba45db64a607cca9d6fee0226889e9a4295
GibborKratos <gibbsonanita@gmail.com>
2023-08-26 11:39:28 +0000

    updated list.h

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+* print_dlistint - prints doubly-linked list
+*@h: address of head node
+*
+*Return:size of list
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
